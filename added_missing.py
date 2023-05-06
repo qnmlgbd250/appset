030484763ee0bdfca9b9dc556cc5016da4a9bb13
if <fukai_sun@kingdee.com>
2023-05-06 14:15:58 +0800

    补充遗漏的cookie

---
+++ added_missing.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import os
+import redis
+import dotenv
+from tokenList import *
+from dotenv import load_dotenv
+
+# 加载 .env 文件
+load_dotenv(".env")
+# 连接Redis数据库
+redis_pool = redis.Redis(host=os.getenv('REDIS_HOST'), port=int(os.getenv('REDIS_PORT')), db=int(os.getenv('REDIS_DB')), password=os.getenv('REDIS_PASS'))
+
+mylist = redis_pool.lrange('cookieList', 0, -1)
+result = [{i:val.decode('utf-8')} for i, val in enumerate(mylist) if val.endswith(b'.com')]
+print(result)
